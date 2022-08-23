2a17f686f6dfcaf5c8489635db3ef34f0d78fd72
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-23 11:50:36 +0800

    Add GPHES

--- GPUI-UWPTS/GPUI-UWPTS.Core/Services/GPDataService.cs
+++ GPUI-UWPTS/GPUI-UWPTS.Core/Services/GPDataService.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using GPUI_UWPTS.Core.Models;
 
 namespace GPUI_UWPTS.Core.Services
 {
     public class GPDataService
     {
+        public List<GPData> LoadData()
+        {
+            List<GPData> gpdatas = new List<GPData>();
+            //在下方添加从文件加载数据的方法
+            
+            //返回值区
+            return gpdatas;
+        }
+
+        public string GPHESservice()
+        {
+            string HESResult = null;
+            return HESResult;
+        }
 
     }
+
+    public class GPHES
+    {
+        
+    }
+
 }
