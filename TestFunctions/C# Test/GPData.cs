950d4664a25f14ce60c37bab6a0ceeb4148ef2ce
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-24 16:49:09 +0800

    Roll Back

---
+++ TestFunctions/C# Test/GPData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace GPDataNS
+{
+    public class GPData
+    {
+        public string DataName
+        {
+            get; set;
+        }
+
+        public string Account
+        {
+            get; set;
+        }
+
+        private string EncPassWord
+        {
+            get; set;
+        }
+
+        public string WebsiteUrl
+        {
+            get; set;
+        }
+
+        public string ReservedData
+        {
+            get; set;
+        }
+
+        public DateTime UpdateTime
+        {
+            get; set;
+        }
+
+        public GPData()
+        {
+            DataName = "GPData_dataname";
+            Account = "GPData_account";
+            EncPassWord = null;
+            WebsiteUrl = null;
+            ReservedData = null;
+            UpdateTime = DateTime.Now;
+        }
+    }
+}
---
+++ TestFunctions/C# Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using GPDataNS;
+using TestFunctions;
+using NETCore.Encrypt;
+
+namespace Program
+{
+    class TestClass
+    {
+        static void Main(string[] args)
+        {
+            var test = "GeorgeDong2352";
+            var testservice = new GPDataService();
+            var gphes = new GPHES(test);
+            gphes.GetSalt();
+            gphes.Mix();
+            Console.WriteLine(EncryptProvider.Sha256(gphes.SaltedStr));
+            Console.WriteLine(testservice.GPHESservice(test));
+            Console.WriteLine(testservice.GPHESservice(test));
+        }
+    }
+}
