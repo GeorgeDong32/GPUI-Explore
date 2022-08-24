0938e3f97e9af8e33c89d8a969a65d2e54c4590e
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-24 11:01:39 +0800

    Test

--- TestFunctions/C# Test/Program.cs
+++ TestFunctions/C# Test/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using GPDataNS;
+using TestFunctions;
+
+namespace Program
+{
+    class TestClass
+    {
+        static void Main(string[] args)
+        {
+            var test = "GeorgeDong2352";
+            var testservice = new GPDataService();
+            Console.WriteLine(testservice.GPHESservice(test));
+        }
+    }
+}
