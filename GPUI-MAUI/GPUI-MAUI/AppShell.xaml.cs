8779a38f283427407ed855476f9ccb1c009bedcd
GeorgeDong32 <georgedong32@foxmail.com>
2022-09-04 10:39:22 +0800

    GPUI-MAUI Create

---
+++ GPUI-MAUI/GPUI-MAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace GPUI_MAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ GPUI-MAUI/GPUI-MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace GPUI_MAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
